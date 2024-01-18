656de59d306eead89925151f9684ebfb24a20e00
Stradex <stradexengine@gmail.com>
2024-01-18 00:00:11 -0300

    OK: get obra social method added in frontend

--- frontend/src/lib/cmds.ts
+++ frontend/src/lib/cmds.ts
@@ -72,6 +72,26 @@ const CMDS: cmdType[] = [
             .catch(e => processCMD(`echo ${e}`));
         }
     },
+    {
+        name: 'get obra social',
+        usage:'get obra social <<codigo | optional>>, <<nombre | optional>>',
+        multiArgs: true,
+        callback: (code: string, nombre: string) => {
+            axios.post(`${GlobalVars.backend_path!}/api/obra_social.php`, {
+                action: 'get',
+                code: code && code.length > 0 ? parseInt(code) : null,
+                nombre: nombre && nombre.length > 0 ? nombre : null
+             },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }).then(r => {
+                processCMD(`echo ${JSON.stringify(r.data, null, 3)}`);
+            })
+            .catch(e => processCMD(`echo ${e}`));
+        }
+    },
 ];
 
 export function addCMD(cmd: cmdType) {
