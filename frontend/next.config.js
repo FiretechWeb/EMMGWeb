a8e138a739aed324631335c7cce6db2d5aa1f93d
Stradex <stradexengine@gmail.com>
2024-01-17 21:49:40 -0300

    OK: added _app.tsx and global css now working again

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,6 @@
 const nextConfig = {
     output: 'standalone',
      trailingSlash: true,
-    //Básicamente este images : { unoptimized: true } fue el que me permitio generar los HTML
     
     images: {
         unoptimized: true,
