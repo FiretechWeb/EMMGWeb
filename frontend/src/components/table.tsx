9d4c077d26032d4723723c2c62b706a8c7d88633
Stradex <stradexengine@gmail.com>
2024-01-19 23:47:49 -0300

    OK: Added AGREGAR Button

--- frontend/src/components/table.tsx
+++ frontend/src/components/table.tsx
@@ -4,40 +4,41 @@ import { InputText } from 'primereact/inputtext';
 import { InputNumber } from 'primereact/inputnumber';
 import { Checkbox } from 'primereact/checkbox';
 import { Calendar } from 'primereact/calendar';
+import { Button } from "primereact/button";
 
 function getComponentFromSQLType(componentKey: string, type: string, name: string) {
     if (type.toLowerCase().includes("varchar")) {
         return (
-            <div key={componentKey}>
-                <label>{name}</label>
+            <div className="m-2" key={componentKey}>
+                <label className="mx-2">{name}: </label>
                 <InputText name={componentKey} ></InputText>
             </div>
         )
     } else if (type.toLowerCase().includes("bigint")) {
         return (
-            <div key={componentKey}>
-                <label>{name}</label>
+            <div className="m-2" key={componentKey}>
+                <label className="mx-2">{name}: </label>
                 <InputNumber name={componentKey} ></InputNumber>
             </div>
         )
     } else if (type.toLowerCase().includes("int")) {
         return (
-            <div key={componentKey}>
-                <label>{name}</label>
+            <div className="m-2" key={componentKey}>
+                <label className="mx-2">{name}: </label>
                 <InputNumber name={componentKey} ></InputNumber>
             </div>
         )
     } else if (type.toLowerCase().includes("tinyint")) {
         return (
-            <div key={componentKey}>
-                <label>{name}</label>
+            <div className="m-2" key={componentKey}>
+                <label className="mx-2">{name}: </label>
                 <Checkbox checked={false}></Checkbox>
             </div>
         )
     } else if (type.toLowerCase().includes("date")) {
         return (
-            <div key={componentKey}>
-                <label>{name}</label>
+            <div className="m-2" key={componentKey}>
+                <label className="mx-2">{name}: </label>
                 <Calendar></Calendar>
             </div>
         )
@@ -53,16 +54,20 @@ interface TableComponentProps {
 
 export default function DBTableComponent(props: TableComponentProps) {
     const [fields, setFields] = useState<{ [fieldName: string]: DBFieldType; }>({});
-    const [tableName, setTableName] = useState("");
+
+    const agregarElemento = (event: any) => {
+        //add element here!
+        event.preventDefault();
+      };
+
     useEffect(() => {
         setFields(JSON.parse(props.jsonData)['fields']);
-        setTableName(props.name)
     }, [props]);
 
     return(
         <div>
-            <h2>{props.name}</h2>
-            <form>
+            <h2 className="text-xl font-bold">{props.name}</h2>
+            <form className="flex flex-col"> 
             {
             Object.keys(fields)
                 .filter(fieldName => fields[fieldName].allow_insert)
@@ -70,6 +75,8 @@ export default function DBTableComponent(props: TableComponentProps) {
                     getComponentFromSQLType(fieldName, fields[fieldName].sql_type, fieldName)
                 ))
             }
+
+            <Button onClick={agregarElemento} className="bg-slate-500 p-1 m-1 self-center place-self-center" label="Agregar"></Button>
             </form>
             <hr />
         </div>
